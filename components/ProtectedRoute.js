a289197f93cde9ac59b920cd4def38c4d24b8665
minamihiroto <kokowatamf@gmail.com>
2023-03-08 11:57:46 +0900

    ログインしていないときに認証必要なページにアクセスしたときにログインページに飛ばす処理

---
+++ components/ProtectedRoute.js
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { useAuth } from "./AuthProvider.js";
+
+const ProtectedRoute = ({ children }) => {
+  const { session, loading } = useAuth();
+  const router = useRouter();
+  
+  //ログインしていないときに認証必要なページにアクセスしたときにログインページに飛ばす処理
+  useEffect(() => {
+    (async () => {
+      if (!session && !loading) {
+        router.push("/login");
+        return;
+      }
+    })();
+  });
+  return <>{session && children}</>;
+};
+
+export default ProtectedRoute;
