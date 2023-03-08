d403e3218d35c577785296458b53e518d843d563
minamihiroto <kokowatamf@gmail.com>
2023-03-08 16:02:13 +0900

    認証ページ

---
+++ pages/login.js
@@ -0,0 +1,40 @@
+import { useRouter } from "next/router";
+import { useAuth } from "../components/AuthProvider";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export default function Login() {
+  const { session, login } = useAuth();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  if (session) {
+    router.push("/");
+  }
+
+  const onSubmit = async (val) => {
+    await login({ email: val.email, password: val.password }).then(() =>
+      router.push("/")
+    );
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>ログイン</h1>
+        <label>メールアドレス</label>
+        <input {...register("email", { required: true })} type="text" />
+        {errors.email && errors.email.message}
+        <label>パスワード</label>
+        <input {...register("password", { required: true })} type="password" />
+        {errors.password && errors.password.message}
+        <button type="submit">送信</button>
+        <Link href="/signup">新規登録へ</Link>
+      </form>
+    </div>
+  );
+}
---
+++ pages/signup.js
@@ -0,0 +1,64 @@
+import { useRouter } from "next/router";
+import { useAuth } from "../components/AuthProvider.js";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export default function Signup() {
+  const { session, signup } = useAuth();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
+
+
+  if (session) {
+    router.push("/");
+  }
+
+  const onSubmit = async (val) => {
+    await signup({ email: val.email, password: val.password })
+      .then(() => {
+        router.push("/login");
+        alert("メールを送信した。ご確認ください。");
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>新規登録</h1>
+        <label>メールアドレス</label>
+        <input
+          {...register("email", { required: "入力してください" })}
+          type="text"
+        />
+        {errors.email && errors.email.message}
+        <label>パスワード</label>
+        <input
+          {...register("password", { required: "入力してください" })}
+          type="password"
+        />
+        {errors.password && errors.password.message}
+        <label>パスワード（確認）</label>
+        <input
+          {...register("passwordConf", {
+            required: "入力してください",
+            validate: (value) => {
+              return (
+                value === getValues("password") ||
+                "メールアドレスが一致しません"
+              );
+            },
+          })}
+          type="password"
+        />
+        {errors.passwordConf && errors.passwordConf.message}
+        <button type="submit">送信</button>
+        <Link href="/login">ログインへ</Link>
+      </form>
+    </div>
+  );
+}
