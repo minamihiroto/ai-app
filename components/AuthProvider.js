90944a6b6cfa854429c54b5d74cda6cccaaf3ece
minamihiroto <kokowatamf@gmail.com>
2023-03-08 16:02:03 +0900

    aupabaseロジック

---
+++ components/AuthProvider.js
@@ -0,0 +1,72 @@
+import { supabase } from "../lib/supabaseClient.js";
+import { useContext, createContext, useEffect, useState } from "react";
+
+const AuthContext = createContext(null);
+const useAuth = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+  const auth = supabase.auth;
+
+  const [loading, setLoading] = useState(true);
+  const [session, setSession] = useState(null);
+
+  //session処理の実行中は画面を表示しないようにする
+  useEffect(() => {
+    let mounted = true;
+
+    (async () => {
+      const {
+        data: { session },
+      } = await auth.getSession();
+      if (mounted) {
+        if (session) {
+          setSession(session);
+        }
+        setLoading(false);
+      }
+    })();
+
+    const {
+      data: { subscription },
+    } = auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      if (_event === "SIGNED_OUT") {
+        setSession(null);
+      }
+    });
+
+    return () => {
+      mounted = false;
+      subscription?.unsubscribe();
+    };
+  });
+
+  const login = async ({ email, password }) => {
+    await auth.signInWithPassword({ email: email, password: password });
+  };
+
+  const signup = async ({ email, password }) => {
+    await auth.signUp({ email: email, password: password });
+  };
+
+  const logout = () => {
+     auth.signOut();
+  };
+
+  const exposed = {
+    session,
+    loading,
+    setLoading,
+    signup,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={exposed}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export { useAuth, AuthProvider };
