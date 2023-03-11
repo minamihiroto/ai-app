2b89b098fa2ca086a05b0d9ea5989db16a1d376e
minamihiroto <kokowatamf@gmail.com>
2023-03-12 03:17:57 +0900

    chartのデザイン修正

--- pages/chart.js
+++ pages/chart.js
@@ -4,16 +4,36 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 const options = {
+  responsive: true,
   scales: {
     y: {
       suggestedMin: -10,
       suggestedMax: 10,
       position: "center",
+      title: {
+        color: "black",
+        align: "center",
+        display: true,
+        text: "y軸のタイトル",
+        minRotation:90,
+        font: {
+          size: 16,
+        },
+      },
     },
     x: {
       suggestedMin: -10,
       suggestedMax: 10,
       position: "center",
+      title: {
+        color: "black",
+        align: "start",
+        display: true,
+        text: "x軸のタイトル",
+        font: {
+          size: 16,
+        },
+      },
     },
   },
   maintainAspectRatio: false,
