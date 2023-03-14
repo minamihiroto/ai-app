88f78f1ef51677de747de874928241a575297208
minamihiroto <kokowatamf@gmail.com>
2023-03-14 14:41:10 +0900

    エラーコード削除

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,57 +9,30 @@ export default async function AI(req, res) {
   async function completion(new_message_text, settings_text, past_messages) {
     if (past_messages == 0 && settings_text != 0) {
       const system = { role: "system", content: settings_text };
-      await past_messages.push(system);
+      await  past_messages.push(system);
     }
 
     const new_message = { role: "user", content: new_message_text };
     await past_messages.push(new_message);
 
-    try {
-      // OpenAI APIを呼び出すコード
-      const result = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: past_messages,
-      });
+    const result = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: past_messages,
+    });
 
-      const response_message = {
-        role: "assistant",
-        content: result.data.choices[0].message.content,
-      };
+    const response_message = {
+      role: "assistant",
+      content: result.data.choices[0].message.content,
+    };
 
-      await past_messages.push(response_message);
-      console.log(result.data.choices[0].message.content);
-      await res.status(200).json({
-        result: result.data.choices[0].message.content,
-        log: past_messages,
-        tokens: result.data.usage,
-      });
-    } catch (e) {
-      if (e instanceof openai.error.APIError) {
-        // APIエラーを処理するコード
-        await res.status(200).json({
-          result: e,
-        });
-      } else if (e instanceof openai.error.APIConnectionError) {
-        // 接続エラーを処理するコード
-        await res.status(200).json({
-          result: e,
-        });
-      } else if (e instanceof openai.error.RateLimitError) {
-        // レート制限エラーを処理するコード
-        await res.status(200).json({
-          result: e,
-        });
-      } else {
-        // その他のエラーを処理するためのカスタムコードを実行する
-        await res.status(200).json({
-          result: e,
-        });
-      }
-    }
+    await past_messages.push(response_message);
+    console.log(result.data.choices[0].message.content);
+
+    await res.status(200).json({ result: result.data.choices[0].message.content, log: past_messages, tokens: result.data.usage });
   }
 
-  const system_settings = `
+  const system_settings = 
+  `
     今からあなたにはJSON形式のデータを出すプログラムになりきっていただきます。
     そのため前置きは要りません。
     下記はあなたの出力例です。必ず[で始まり、]で終わってください。dataが持つ値にはxとyがありますが、それぞれ1つづつ持つこと。
